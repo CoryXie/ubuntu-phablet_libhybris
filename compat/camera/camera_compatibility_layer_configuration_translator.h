096252add6ed5095ad8b4f44d5386de456d42c1d
Thomas Voß <thomas.voss@canonical.com>
2012-10-11 18:23:58 +0200

    Add getters and a way to stream images to a texture.

---
+++ compat/camera/camera_compatibility_layer_configuration_translator.h
@@ -0,0 +1,154 @@
+/*
+ * Copyright (C) 2012 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by: Thomas Voss <thomas.voss@canonical.com>
+ */
+
+#ifndef CAMERA_COMPATIBILITY_LAYER_CONFIGURATION_TRANSLATOR_H_
+#define CAMERA_COMPATIBILITY_LAYER_CONFIGURATION_TRANSLATOR_H_
+
+#include "camera_compatibility_layer_capabilities.h"
+
+#include <camera/CameraParameters.h>
+#include <utils/KeyedVector.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+static const char* effect_modes[] = 
+{
+    android::CameraParameters::EFFECT_NONE,
+    android::CameraParameters::EFFECT_MONO,
+    android::CameraParameters::EFFECT_NEGATIVE,
+    android::CameraParameters::EFFECT_SOLARIZE,
+    android::CameraParameters::EFFECT_SEPIA,
+    android::CameraParameters::EFFECT_POSTERIZE,
+    android::CameraParameters::EFFECT_WHITEBOARD,
+    android::CameraParameters::EFFECT_BLACKBOARD,
+    android::CameraParameters::EFFECT_AQUA
+};
+
+android::KeyedVector<android::String8, EffectMode> init_effect_modes_lut()
+{
+    android::KeyedVector<android::String8, EffectMode> m;
+    m.add(android::String8(android::CameraParameters::EFFECT_NONE), EFFECT_MODE_NONE); 
+    m.add(android::String8(android::CameraParameters::EFFECT_MONO), EFFECT_MODE_MONO);
+    m.add(android::String8(android::CameraParameters::EFFECT_NEGATIVE), EFFECT_MODE_NEGATIVE);
+    m.add(android::String8(android::CameraParameters::EFFECT_SOLARIZE), EFFECT_MODE_SOLARIZE);
+    m.add(android::String8(android::CameraParameters::EFFECT_SEPIA), EFFECT_MODE_SEPIA);
+    m.add(android::String8(android::CameraParameters::EFFECT_POSTERIZE), EFFECT_MODE_POSTERIZE);
+    m.add(android::String8(android::CameraParameters::EFFECT_WHITEBOARD), EFFECT_MODE_WHITEBOARD);
+    m.add(android::String8(android::CameraParameters::EFFECT_BLACKBOARD), EFFECT_MODE_BLACKBOARD);
+    m.add(android::String8(android::CameraParameters::EFFECT_AQUA), EFFECT_MODE_AQUA);
+
+    return m;
+}
+
+static android::KeyedVector<android::String8, EffectMode> effect_modes_lut = init_effect_modes_lut();
+
+static const char* white_balance_modes[] = {
+    android::CameraParameters::WHITE_BALANCE_AUTO,
+    android::CameraParameters::WHITE_BALANCE_DAYLIGHT,
+    android::CameraParameters::WHITE_BALANCE_CLOUDY_DAYLIGHT,
+    android::CameraParameters::WHITE_BALANCE_FLUORESCENT,
+    android::CameraParameters::WHITE_BALANCE_INCANDESCENT
+};
+
+android::KeyedVector<android::String8, WhiteBalanceMode> init_white_balance_modes_lut()
+{
+    android::KeyedVector<android::String8, WhiteBalanceMode> m;
+    m.add(android::String8(android::CameraParameters::WHITE_BALANCE_AUTO), WHITE_BALANCE_MODE_AUTO); 
+    m.add(android::String8(android::CameraParameters::WHITE_BALANCE_DAYLIGHT), WHITE_BALANCE_MODE_DAYLIGHT);
+    m.add(android::String8(android::CameraParameters::WHITE_BALANCE_CLOUDY_DAYLIGHT), WHITE_BALANCE_MODE_CLOUDY_DAYLIGHT);
+    m.add(android::String8(android::CameraParameters::WHITE_BALANCE_FLUORESCENT), WHITE_BALANCE_MODE_FLUORESCENT);
+    m.add(android::String8(android::CameraParameters::WHITE_BALANCE_INCANDESCENT), WHITE_BALANCE_MODE_INCANDESCENT);
+    return m;
+}
+
+static android::KeyedVector<android::String8, WhiteBalanceMode> white_balance_modes_lut = init_white_balance_modes_lut();
+
+static const char* flash_modes[] = 
+{
+    android::CameraParameters::FLASH_MODE_OFF,
+    android::CameraParameters::FLASH_MODE_AUTO,
+    android::CameraParameters::FLASH_MODE_ON,
+    android::CameraParameters::FLASH_MODE_TORCH
+};
+
+static android::KeyedVector<android::String8, FlashMode> init_flash_modes_lut()
+{
+    android::KeyedVector<android::String8, FlashMode> m;
+    m.add(android::String8(android::CameraParameters::FLASH_MODE_OFF), FLASH_MODE_OFF);
+    m.add(android::String8(android::CameraParameters::FLASH_MODE_AUTO), FLASH_MODE_AUTO);
+    m.add(android::String8(android::CameraParameters::FLASH_MODE_ON), FLASH_MODE_ON);
+    m.add(android::String8(android::CameraParameters::FLASH_MODE_TORCH), FLASH_MODE_TORCH);
+    
+    return m;
+}
+
+static android::KeyedVector<android::String8, FlashMode> flash_modes_lut = init_flash_modes_lut();
+
+static const char* scene_modes[] = {
+    android::CameraParameters::SCENE_MODE_AUTO,
+    android::CameraParameters::SCENE_MODE_ACTION,
+    android::CameraParameters::SCENE_MODE_NIGHT,
+    android::CameraParameters::SCENE_MODE_PARTY,
+    android::CameraParameters::SCENE_MODE_SUNSET
+};
+
+static android::KeyedVector<android::String8, SceneMode> init_scene_modes_lut()
+{
+    android::KeyedVector<android::String8, SceneMode> m;
+    m.add(android::String8(android::CameraParameters::SCENE_MODE_AUTO), SCENE_MODE_AUTO);
+    m.add(android::String8(android::CameraParameters::SCENE_MODE_ACTION), SCENE_MODE_ACTION);
+    m.add(android::String8(android::CameraParameters::SCENE_MODE_NIGHT), SCENE_MODE_NIGHT);
+    m.add(android::String8(android::CameraParameters::SCENE_MODE_PARTY), SCENE_MODE_PARTY);
+    m.add(android::String8(android::CameraParameters::SCENE_MODE_SUNSET), SCENE_MODE_SUNSET);
+    
+    return m;
+}
+
+static android::KeyedVector<android::String8, SceneMode> scene_modes_lut = init_scene_modes_lut();
+
+static const char* auto_focus_modes[] = {
+    android::CameraParameters::FOCUS_MODE_FIXED,
+    android::CameraParameters::FOCUS_MODE_CONTINUOUS_VIDEO,
+    android::CameraParameters::FOCUS_MODE_AUTO,
+    android::CameraParameters::FOCUS_MODE_MACRO,
+    android::CameraParameters::FOCUS_MODE_CONTINUOUS_PICTURE,
+    android::CameraParameters::FOCUS_MODE_INFINITY
+};
+
+static android::KeyedVector<android::String8, AutoFocusMode> init_auto_focus_modes_lut()
+{
+    android::KeyedVector<android::String8, AutoFocusMode> m;
+    m.add(android::String8(android::CameraParameters::FOCUS_MODE_FIXED), AUTO_FOCUS_MODE_OFF);
+    m.add(android::String8(android::CameraParameters::FOCUS_MODE_CONTINUOUS_VIDEO), AUTO_FOCUS_MODE_CONTINUOUS_VIDEO);
+    m.add(android::String8(android::CameraParameters::FOCUS_MODE_AUTO), AUTO_FOCUS_MODE_AUTO);
+    m.add(android::String8(android::CameraParameters::FOCUS_MODE_MACRO), AUTO_FOCUS_MODE_MACRO);
+    m.add(android::String8(android::CameraParameters::FOCUS_MODE_CONTINUOUS_PICTURE), AUTO_FOCUS_MODE_CONTINUOUS_PICTURE);
+    m.add(android::String8(android::CameraParameters::FOCUS_MODE_INFINITY), AUTO_FOCUS_MODE_INFINITY);
+    
+    return m;
+}
+
+static android::KeyedVector<android::String8, AutoFocusMode> auto_focus_modes_lut = init_auto_focus_modes_lut();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
