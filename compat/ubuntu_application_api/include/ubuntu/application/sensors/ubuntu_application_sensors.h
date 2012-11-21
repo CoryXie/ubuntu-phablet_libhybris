b0a6f514b4b58ecec3467b69d264545deca3d616
Thomas Voß <thomas.voss@canonical.com>
2012-11-21 17:32:07 +0100

    compat/ubuntu-application-api: Add C api for accelerometer access.

---
+++ compat/ubuntu_application_api/include/ubuntu/application/sensors/ubuntu_application_sensors.h
@@ -0,0 +1,51 @@
+/*
+ * Copyright © 2012 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by: Thomas Voß <thomas.voss@canonical.com>
+ */
+#ifndef UBUNTU_APPLICATION_SENSORS_C_API_H_
+#define UBUNTU_APPLICATION_SENSORS_C_API_H_
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    typedef struct
+    {
+        int64_t timestamp;
+
+        float acceleration_x;
+        float acceleration_y;
+        float acceleration_z;
+    } ubuntu_sensor_accelerometer_reading;
+
+    typedef struct
+    {
+        typedef void (*on_new_accelerometer_reading)(ubuntu_sensor_accelerometer_reading* reading, void* context);
+
+        on_new_accelerometer_reading on_new_accelerometer_reading_cb;
+
+        void* context;
+    } ubuntu_accelerometer_observer;
+
+    void ubuntu_sensor_install_accelerometer_observer(ubuntu_accelerometer_observer* observer);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // UBUNTU_APPLICATION_SENSORS_C_API_H_
---
+++ compat/ubuntu_application_api/src/hybris/test_sensors_c_api.cpp
@@ -0,0 +1,45 @@
+/*
+ * Copyright © 2012 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by: Thomas Voß <thomas.voss@canonical.com>
+ */
+
+#include <ubuntu/application/sensors/ubuntu_application_sensors.h>
+
+#include <stdio.h>
+#include <string.h>
+
+void on_new_reading(ubuntu_sensor_accelerometer_reading* reading, void*)
+{
+    printf("%s \n", __PRETTY_FUNCTION__);
+    printf("\ttime: %d\n", reading->timestamp);
+    printf("\tx: %f\n", reading->acceleration_x);
+    printf("\ty: %f\n", reading->acceleration_y);
+    printf("\tz: %f\n", reading->acceleration_z);
+}
+
+int main(int argc, char** argv)
+{
+    ubuntu_accelerometer_observer observer;
+    memset(&observer, 0, sizeof(observer));
+
+    observer.on_new_accelerometer_reading_cb = on_new_reading;
+
+    ubuntu_sensor_install_accelerometer_observer(&observer);
+
+    while(true)
+    {
+    }
+}
