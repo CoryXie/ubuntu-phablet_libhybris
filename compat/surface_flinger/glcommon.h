66fa5d4666c968aad4b9f3b57bd55e7dedaca2ea
Michael Terry <michael.terry@canonical.com>
2013-04-11 10:23:59 -0400

    remove more unnecessary GLES2 includes; switch one GLfloat to float directly

--- compat/surface_flinger/glcommon.h
+++ compat/surface_flinger/glcommon.h
@@ -15,10 +15,6 @@
  *
  * Authored by: Thomas Voß <thomas.voss@canonical.com>
  */
-#include <GLES2/gl2.h>
-#include <GLES2/gl2ext.h>
-#include <EGL/egl.h>
-#include <EGL/eglext.h>
 
 #include <utils/misc.h>
 
